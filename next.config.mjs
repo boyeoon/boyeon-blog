b5979e4e91d1b51a26ac0671131f50f70b04f50a
Boyeon Jang <uko010199@gmail.com>
2024-10-13 21:19:27 +0900

    fix: MDX 지원을 위한 파일 내용 수정
    - next.config.mjs 내용 수정

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// Import the MDX plugin
+const withMDX = require("@next/mdx")();
 
-export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // MDX 플러그인 추가
+  pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
+};
+
+// Use the MDX plugin
+module.exports = withMDX(nextConfig);

19ae7d01819942d3722d95c0fe6499e3d1d1b4f6
Boyeon Jang <uko010199@gmail.com>
2024-10-18 19:24:28 +0900

    feat: 블로그 홈 페이지 구현 및 카테고리 링크 추가

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,28 @@
-// import { categories } from "@/categories";
-// import { Pagination } from "@/components/pagination";
-// import { Posts } from "@/components/posts";
-// import { getPaginatedPosts, postsPerPage } from "@/posts";
+import { categories } from "@/categories";
+import { Pagination } from "@/components/pagination";
+import { Posts } from "@/components/posts";
+import { getPaginatedPosts, postsPerPage } from "@/posts";
 
 import React from "react";
 
 export default async function Home() {
-  // const { posts, total } = await getPaginatedPosts({ page: 1, limit: postsPerPage });
+  const { posts, total } = await getPaginatedPosts({ page: 1, limit: postsPerPage });
 
   return (
-    <div>
+    <main>
       <h1>Next.js MDX Blog</h1>
-      
-    </div>
+      <Posts posts={posts} />
+
+      <Pagination baseUrl="/page" page={1} perPage={postsPerPage} total={total} />
+
+      <h2>Categories</h2>
+      <ul>
+        {categories.map((cat) => (
+          <li key={cat}>
+            <a href={`category/${cat}`}>{cat}</a>
+          </li>
+        ))}
+      </ul>
+    </main>
   );
 }

f743892a7212e7917e31ee6f2528236fc7719982
Boyeon Jang <uko010199@gmail.com>
2024-10-22 23:59:03 +0900

    feat: 다크모드 버튼 위치 수정

--- app/components/header/index.tsx
+++ app/components/header/index.tsx
@@ -4,19 +4,21 @@ import DarkMode from "@/components/darkmode";
 export default function Header() {
   return (
     <header className="pl-16">
-      <nav className="flex font-bold gap-x-4">
-        <Link className="rounded p-[0.2rem] no-underline hover:bg-slate-400" href="/">
-          Home
-        </Link>
-        <Link className="rounded p-[0.2rem] no-underline hover:bg-slate-400" href="/blog">
-          Blog
-        </Link>
-        <Link className="rounded p-[0.2rem] no-underline hover:bg-slate-400" href="/project">
-          Projects
-        </Link>
-        <Link className="rounded p-[0.2rem] no-underline hover:bg-slate-400" href="/resume">
-          Resume
-        </Link>
+      <nav className="flex justify-between font-bold gap-x-4">
+       <div className="flex gap-x-4">
+          <Link className="rounded p-[0.2rem] no-underline hover:bg-slate-400" href="/">
+            Home
+          </Link>
+          <Link className="rounded p-[0.2rem] no-underline hover:bg-slate-400" href="/blog">
+            Blog
+          </Link>
+          <Link className="rounded p-[0.2rem] no-underline hover:bg-slate-400" href="/project">
+            Projects
+          </Link>
+          <Link className="rounded p-[0.2rem] no-underline hover:bg-slate-400" href="/resume">
+            Resume
+          </Link>
+        </div>
         <DarkMode />
       </nav>
     </header>

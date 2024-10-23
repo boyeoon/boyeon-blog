a85ded524d8a87ddc80de37de5b0d6cfbfdb0ede
Boyeon Jang <uko010199@gmail.com>
2024-10-24 00:45:24 +0900

    feat: 카테고리 안보이게 임시 주석처리

--- app/components/posts/index.tsx
+++ app/components/posts/index.tsx
@@ -4,16 +4,17 @@ import Link from "next/link";
 export function Posts({ posts }: { posts: Post[] }) {
   return (
     <ol>
-      {posts.map(({ slug, title, publishDate, categories }) => (
+      {/* {posts.map(({ slug, title, publishDate, categories }) => ( */}
+      {posts.map(({ slug, title, publishDate }) => (
         <li key={slug}>
-          <h2>
+          <h2 className="mb-0">
             <Link href={`/${slug}`}>{title}</Link>
           </h2>
-          <p>
-            <strong>Published:</strong>{" "}
+          <p className="pl-1">
+            {/* <strong>Published:</strong>{" "} */}
             {new Date(publishDate).toLocaleDateString()}{" "}
-            <strong>Categories:</strong>{" "}
-            {categories.map((cat, i) => `${i ? ", " : ""}${cat}`)}
+            {/* <strong>Categories:</strong>{" "}
+            {categories.map((cat, i) => `${i ? ", " : ""}${cat}`)} */}
           </p>
         </li>
       ))}

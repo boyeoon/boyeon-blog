64d4435f4a7cd6a15df43256379540f06e9e1abe
Boyeon Jang <uko010199@gmail.com>
2024-10-24 20:37:31 +0900

    feat: main 페이지 임시 수정

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,18 @@
-import { Pagination } from "@/components/pagination";
-import { Posts } from "@/components/posts";
-import { getPaginatedPosts, postsPerPage } from "@/posts";
+// import { Pagination } from "@/components/pagination";
+// import { Posts } from "@/components/posts";
+// import { getPaginatedPosts, postsPerPage } from "@/posts";
 
 import React from "react";
 
 export default async function Home() {
-  const { posts, total } = await getPaginatedPosts({ page: 1, limit: postsPerPage });
+  // const { posts, total } = await getPaginatedPosts({ page: 1, limit: postsPerPage });
 
   return (
     <main className="p-16">
-      <h1>Boyeon Jang</h1>
-      <Posts posts={posts} />
+      <h1 className="flex justify-center text-[7rem]">Boyeon Jang</h1>
+      {/* <Posts posts={posts} /> */}
 
-      <Pagination baseUrl="/page" page={1} perPage={postsPerPage} total={total} />
+      {/* <Pagination baseUrl="/page" page={1} perPage={postsPerPage} total={total} /> */}
     </main>
   );
 }

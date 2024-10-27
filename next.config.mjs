d204e2722bf61e5f8e42acb5104451d2ffe3fdff
Boyeon Jang <uko010199@gmail.com>
2024-10-27 18:15:15 +0900

    feat: rehype 플러그인 rehype-prism-plus 패키지 설치

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,11 @@
 import nextMDX from "@next/mdx";
 import remarkGfm from "remark-gfm";
+import rehypePrism from "rehype-prism-plus";
 
 const withMDX = nextMDX({
   options: {
     remarkPlugins: [remarkGfm],
-    rehypePlugins: [],
+    rehypePlugins: [rehypePrism],
   },
 });
 
